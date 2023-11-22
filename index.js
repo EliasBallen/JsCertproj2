37b2cc4e6c1ed9bb82ba5f37c2e096454a2934a2
EliasBallen <@EliasBallen>
2023-11-22 10:01:48 -0500

    2nd proj test build

---
+++ index.js
@@ -0,0 +1,31 @@
+
+//inicialize project
+const express = require('express');
+const app = express();
+
+
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus:200}));
+
+
+
+app.use(express.static('public'));
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname + "/views/index.html")
+})
+app.get("/api/whoami",function(req,res){
+    console.log(req.headers)
+    res.json({
+        ipaddress:req.ip,
+        language:req.headers['accept-language'],
+        software:req.headers['user-agent'],
+    })
+})
+
+
+var listener = app.listen(process.env.PORT||
+    3000, function(){
+        console.log("Your app is listening on port " 
+        + listener.address().port);
+    })
